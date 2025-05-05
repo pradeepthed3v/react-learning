af8f9309c9a33350dae5299db516c418ccc85f6e
pradeepthed3v <pradeepsiva013@gmail.com>
2025-05-05 17:41:20 +0530

    react chatbot added

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,35 +1,140 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Chatbot } from 'supersimpledev';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function ChatInput({chatMessages,setChatMessages}) {
+  const [inputText,setInputText]=useState('');
 
+  function saveInputText(event){
+    setInputText(event.target.value);
+
+  }
+  function sendMessage(){
+    const newChatMessages= [
+    ...chatMessages,
+    {
+        message:inputText,
+        sender:'user',
+        id: crypto.randomUUID()
+      }
+    ];
+    
+    setChatMessages(newChatMessages);
+
+    const response=Chatbot.getResponse(inputText);
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message:response,
+        sender:'robot',
+        id:crypto.randomUUID()
+      }
+    ]);
+
+
+
+    setInputText('')
+
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input 
+        placeholder="Send a message" 
+        size="30"
+        onChange={saveInputText}
+        value={inputText}
+      />
+      <button onClick={sendMessage}>send</button>
     </>
-  )
+  );
+}
+
+function ChatMessage({message,sender}){
+  //const message= props.message;
+  //const sender = props.sender;
+  //const {message, sender} = props;
+
+ /* if(sender==='robot')
+  return(
+    <div>
+      <img src="robot.png"width="50"/>
+     {message} 
+     
+    </div>
+
+);*/
+  return (
+    <div>
+      {sender === 'robot' && (
+        <img src="robot.png"width="50"/>)}
+     {message} 
+     {sender === 'user' && (
+      <img src="user.png"width="50"/>)}
+    </div>
+
+  );
+
+}
+function ChatMessages({chatMessages}){
+ return(
+      <>
+    
+      
+        {chatMessages.map((chatMessage)=>{
+          return(
+            <ChatMessage
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id} 
+            />
+          );
+        })}
+      </>
+      );
+
+}
+
+function App(){
+  const [chatMessages, setChatMessgaes] = useState([{
+    message : 'hello chatbot',
+    sender : 'user',
+    id : 'id1'
+  },{
+    message : 'Hello!How can I help you?',
+    sender : 'robot',
+    id : 'id2'
+  },{
+    message : 'Can you get me todays date?',
+    sender : 'user',
+    id : 'id3'
+  },{
+    message:'Today is April 22',
+    sender : 'robot',
+    id : 'id4'
+  }]);
+   //const[ChatMessgaes,setChatMessages]=array;
+  //const ChatMessages= array[0];
+  //const setChatMessages= array[1];
+
+
+
+ return(
+      <>
+      <ChatInput 
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessgaes}
+      />
+
+      <ChatMessages
+       chatMessages={chatMessages}
+      />
+     
+      
+
+    </>
+
+  );
+
 }
 
 export default App
